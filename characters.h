f5edff1cabbab41a189f2a286e109785b61af336
Nathan van Doorn <nathan@myrtlesoftware.com>
2019-05-06 20:46:12 +0100

    Add some letter forms
    This is enough to writ the words "HOLD", "NEXT", "SCORE", "HIGH", and "TETRIS"

--- characters.h
+++ characters.h
@@ -79,3 +79,108 @@ const character character9 =
 , { 0, 0, 1}
 , { 1, 1, 0}
 };
+
+const character characterC =
+{ { 0, 1, 1}
+, { 1, 0, 0}
+, { 1, 0, 0}
+, { 1, 0, 0}
+, { 0, 1, 1}
+};
+
+const character characterD =
+{ { 1, 1, 0}
+, { 1, 0, 1}
+, { 1, 0, 1}
+, { 1, 0, 1}
+, { 1, 1, 0}
+};
+
+const character characterE =
+{ { 1, 1, 1}
+, { 1, 0 ,0}
+, { 1, 1, 0}
+, { 1, 0, 0}
+, { 1, 1, 1}
+};
+
+const character characterG =
+{ { 0, 1, 1}
+, { 1, 0, 0}
+, { 1, 0, 0}
+, { 1, 0, 1}
+, { 0, 1, 1}
+};
+
+const character characterH =
+{ { 1, 0, 1}
+, { 1, 0, 1}
+, { 1, 1, 1}
+, { 1, 0, 1}
+, { 1, 0, 1}
+};
+
+const character characterI =
+{ { 1, 1, 1}
+, { 0, 1, 0}
+, { 0, 1, 0}
+, { 0, 1, 0}
+, { 1, 1, 1}
+};
+
+const character characterL =
+{ { 1, 0, 0}
+, { 1, 0, 0}
+, { 1, 0, 0}
+, { 1, 0, 0}
+, { 1, 1, 1}
+};
+
+/* this one's a bit dodgy */
+const character characterN =
+{ { 0, 0, 1}
+, { 1, 0, 1}
+, { 1, 1, 1}
+, { 1, 0, 1}
+, { 1, 0, 0}
+};
+
+const character characterO =
+{ { 1, 1, 1}
+, { 1, 0, 1}
+, { 1, 0, 1}
+, { 1, 0, 1}
+, { 1, 1, 1}
+};
+
+const character characterR =
+{ { 1, 1, 0}
+, { 1, 0, 1}
+, { 1, 1, 0}
+, { 1, 0, 1}
+, { 1, 0, 1}
+};
+
+const character characterS =
+{ { 0, 1, 1}
+, { 1, 0, 0}
+, { 0, 1, 0}
+, { 0, 0, 1}
+, { 1, 1, 0}
+};
+
+const character characterT =
+{ { 1, 1, 1}
+, { 0, 1, 0}
+, { 0, 1, 0}
+, { 0, 1, 0}
+, { 0, 1, 0}
+};
+
+const character characterX =
+{ { 1, 0, 1}
+, { 1, 0, 1}
+, { 0, 1, 0}
+, { 1, 0, 1}
+, { 1, 0, 1}
+};
